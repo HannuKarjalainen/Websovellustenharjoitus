d8971391184c241a806e938d41b1c862eb9aea7f
HannuKarjalainen <143908228+HannuKarjalainen@users.noreply.github.com>
2024-03-05 11:12:58 +0200

    Lisätty vielä endpointtien selitteet kommentteihin

--- src/main/java/com/cardgame/bigorsmall/CardGameController.java
+++ src/main/java/com/cardgame/bigorsmall/CardGameController.java
@@ -18,38 +18,38 @@ public class CardGameController {
         this.cardGame = cardGame;
     }
 
-    @GetMapping("/") 
+    @GetMapping("/")  //Tämä endpoint oli alunperin tarkoitus näyttää ohjeet, mutta tein sitten kuitenkin graafisen UI:n tehtävänannon vastaisesti. Ohjeet ohjattu /guide.
     public String index() throws IOException {
         Resource resource = new ClassPathResource("static/index.html");
         return StreamUtils.copyToString(resource.getInputStream(), Charset.defaultCharset());
     }
 
-    @GetMapping("/draw") 
+    @GetMapping("/draw")  // Tämä endpoint vetää kortin pakasta ja palauttaa sen. Jos pakka on tyhjä, ilmoitetaan siitä käyttäjälle.
     public String drawCard() {
         return cardGame.drawCard();
     }
 
-    @PostMapping("/guess") 
+    @PostMapping("/guess") // Tämä endpoint ottaa vastaan pelaajan arvauksen ("isompi" tai "pienempi") ja vertaa sitä seuraavaan korttiin pakassa. Se palauttaa viestin, joka ilmoittaa, menikö arvaus oikein vai väärin, ja näyttää myös seuraavan kortin. Lisäksi se pitää kirjaa oikeista arvauksista ja ilmoittaa niiden määrän.
     public String makeGuess(@RequestBody String guess) {
         return cardGame.makeGuess(guess);
     }
 
-    @GetMapping("/start") 
+    @GetMapping("/start") //Tämä endpoint aloittaa uuden pelin. Se luo täyden pakan, sekoittaa sen uudestaan ja vetää ensimmäisen kortin. Se palauttaa viestin, jossa kerrotaan, että peli on aloitettu, ja näyttää ensimmäisen kortin.
     public String startGame() {
         return cardGame.startGame();
     }
 
-    @GetMapping("/guide") 
+    @GetMapping("/guide") // Tämä endpoint on ohjeita varten pienempi tai isompi kirjoitetaan postmania käyttäessä post bodyyn raw tekstinä
         public String getGuide() {
             return cardGame.getGuide();
         }
 
-    @GetMapping("/currentCard")
+    @GetMapping("/currentCard") // Getter currentCard-muuttujalle
     public String getCurrentCard() {
         return cardGame.getCurrentCard();
     }
 
-    @GetMapping("/correctGuesses")
+    @GetMapping("/correctGuesses") // Tämä endpoint on tehty arvausten määrän palauttamiseksi
     public String getCorrectGuesses() {
         return cardGame.getCorrectGuesses();
     }
